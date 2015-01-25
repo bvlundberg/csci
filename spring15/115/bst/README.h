80aaffcbd443cd0ee57d72445ee688d5eb5d9395
bvlundberg <bvlundberg@gmail.com>
2015-01-24 18:35:41 -0800

    Read me file created to explain purpose andstructure of the project

---
+++ spring15/115/bst/README.h
@@ -0,0 +1,28 @@
+/*	Author: Brandon Lundberg
+	File Name: README.h
+	Purpose: Implementing a binary search tree - Basic documentation
+	Date: 24 Jan 2015
+*/
+
+/*	Neccesary files:
+		Node.h - Implementation of the node class
+			Members:
+				Integer value for the node
+				Node pointer for left child
+				Node pointer for right child
+			Methods:
+				Constructor{
+					set value for the Node
+				}
+		BST.h - Implementation of the bst class
+			Members:
+				Node to hold the root of the BST
+			Methods:
+				insert - adds a Node to the tree
+				delete - removes a Node from the tree
+				find - searhces the tree for a specific Node
+		Main.cpp - The driving file for the BST
+			- Create the BST
+			- Insert, delete and find nodes
+			- print results of each function
+*/
