62c7a843cf4bbf05c72e8c02359a3ba4a75051ea
bvlundberg <bvlundberg@gmail.com>
2015-02-23 08:36:27 -0800

    Added header file and both cpp files

---
+++ spring15/115/lab4/driver.cpp
@@ -0,0 +1,12 @@
+/*	Author:		Brandon Lundberg
+	File Name:	driver.cpp
+	Purpose: Runs the test cases for the linked list implementation
+	Date: 23 February 2015
+*/
+
+#include "List.h"
+
+int main(){
+
+	return 0;
+}
---
+++ spring15/115/lab4/lists.cpp
@@ -0,0 +1,19 @@
+/*	Author:		Brandon Lundberg
+	File Name:	lists.cpp
+	Purpose: Holds the abstract classes for a link and linkedlist
+	Date: 23 February 2015
+*/
+
+class Link{
+	private:
+		Link freeList;
+		int activeNodes;
+		int freeNodes;
+
+	public:
+
+}
+
+class LinkedList{
+	
+}
